e4b731b85cf3f672a6a7bafaea19f398e4d3c7d1
Mysticpasta1 <ethana5tc@outlook.com>
2023-05-04 09:12:50 -0500

    chest bubbles

--- src/main/java/azmalent/chestbubbles/mixin/ChestTileEntityMixin.java
+++ src/main/java/azmalent/chestbubbles/mixin/ChestTileEntityMixin.java
@@ -2,36 +2,43 @@ package azmalent.chestbubbles.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraft.world.level.block.entity.EnderChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChestBlockEntity.class)
 public abstract class ChestTileEntityMixin {
 
+    private static int e, a;
+
     @Inject(method = "lidAnimateTick", at = @At(value = "TAIL"))
     private static void makeBubbleParticles(Level level, BlockPos blockPos, BlockState blockState, ChestBlockEntity self, CallbackInfo ci) {
-        if (level instanceof ServerLevel serverLevel) {
-            if (blockState.getValue(BlockStateProperties.WATERLOGGED) && self.getOpenNess(serverLevel.getServer().getAverageTickTime()) > 0) {
-                RandomSource random = level.getRandom();
+        if (blockState.getValue(BlockStateProperties.WATERLOGGED) && self.triggerEvent((int) self.getOpenNess((float) e), a)) {
+            RandomSource random = level.getRandom();
 
-                for (int i = 0; i < 1; i++) {
-                    double speed = 0.5 + random.nextDouble() * 0.04;
+            for (int i = 0; i < 1; i++) {
+                double speed = 0.5 + random.nextDouble() * 0.04;
 
-                    level.addParticle(ParticleTypes.BUBBLE_COLUMN_UP, blockPos.getX() + 0.5f, blockPos.getY(), blockPos.getZ() + 0.5f, 0, speed, 0);
-                }
-
-                level.playSound(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.BUBBLE_COLUMN_WHIRLPOOL_AMBIENT, SoundSource.BLOCKS, 0.6F + random.nextFloat() * 0.4F, 0.9F + random.nextFloat() * 0.15F);
+                level.addParticle(ParticleTypes.BUBBLE_COLUMN_UP, blockPos.getX() + 0.5f, blockPos.getY(), blockPos.getZ() + 0.5f, 0, speed, 0);
             }
+
+            level.playSound(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.BUBBLE_COLUMN_WHIRLPOOL_AMBIENT, SoundSource.BLOCKS, 0.6F + random.nextFloat() * 0.4F, 0.9F + random.nextFloat() * 0.15F);
         }
+    }
+
+    @Inject(method = "triggerEvent", at = @At("TAIL"))
+    public void getInts(int e2, int a2, CallbackInfoReturnable<Boolean> cir) {
+        a = a2;
+        e = e2;
     }
 }
--- src/main/java/azmalent/chestbubbles/mixin/IronChestTileEntityMixin.java
+++ src/main/java/azmalent/chestbubbles/mixin/IronChestTileEntityMixin.java
@@ -7,27 +7,37 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.EnderChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractIronChestBlockEntity.class)
 public class IronChestTileEntityMixin {
-    /*@Inject(method = "lidAnimateTick", at = @At(value = "TAIL"))
-    private static void makeBubbleParticles(Level level, BlockPos blockPos, BlockState blockState, AbstractIronChestBlockEntity self, CallbackInfo ci) {
-        if (blockState.getValue(BlockStateProperties.WATERLOGGED)) {
+    private static int e, a;
+
+    @Inject(method = "lidAnimateTick", at = @At(value = "TAIL"))
+    private static void makeBubbleParticles(Level level, BlockPos blockPos, BlockState blockState, AbstractIronChestBlockEntity chestBlockEntity, CallbackInfo ci) {
+        if (blockState.getValue(BlockStateProperties.WATERLOGGED) && self.triggerEvent((int) self.getOpenNess((float) e), a)) {
             RandomSource random = level.getRandom();
 
             for (int i = 0; i < 1; i++) {
                 double speed = 0.5 + random.nextDouble() * 0.04;
 
-                level.addParticle(ParticleTypes.BUBBLE_COLUMN_UP, blockPos.getX(), blockPos.getY(), blockPos.getZ(), 0, speed, 0);
+                level.addParticle(ParticleTypes.BUBBLE_COLUMN_UP, blockPos.getX() + 0.5f, blockPos.getY(), blockPos.getZ() + 0.5f, 0, speed, 0);
             }
 
             level.playSound(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.BUBBLE_COLUMN_WHIRLPOOL_AMBIENT, SoundSource.BLOCKS, 0.6F + random.nextFloat() * 0.4F, 0.9F + random.nextFloat() * 0.15F);
         }
-    }*/
+    }
+
+    @Inject(method = "triggerEvent", at = @At("TAIL"))
+    public void getInts(int e2, int a2, CallbackInfoReturnable<Boolean> cir) {
+        a = a2;
+        e = e2;
+    }
 }
